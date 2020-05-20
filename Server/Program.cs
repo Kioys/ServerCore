21793c4922bd8543af7d39b1a55f8c341ebc8d1e
Matias Arratibel <44926624+Kioys@users.noreply.github.com>
2020-05-20 19:20:55 -0400

    ESTO ES UN COMMIT DE PRUEBA
    commit de prueba

--- Server/Program.cs
+++ Server/Program.cs
@@ -21,7 +21,7 @@ namespace Server
             'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
             '1','2','3','4','5','6','7','8','9','0','_','@','%','$','#','/','*','?','^','&','!','=','-','|','$','='
         };
-
+        public int ESTAESUNAVARIABLECOCHINA_DE_PRUEBA;
         static void Main(string[] args)
         {
 
